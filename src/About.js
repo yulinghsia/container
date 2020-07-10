45f632492da8027df6d1aaf0cf16fb68cf367453
yx70121 <xiayuling@johndeere.com>
2020-07-10 11:38:47 -0500

    package.json and env changes

--- src/About.js
+++ src/About.js
@@ -4,11 +4,9 @@ import './about.css';
 const About = () => (
   <main id="about">
     <h2>About this site</h2>
-    <p>
-      This website was created by{' '}
-      <a href="https://twitter.com/thecamjackson">Cam Jackson</a> to demonstrate
-      just one way that micro frontends can be implemented.
-    </p>
+      <p>
+      This website is a micro frontend React app. It is deployed on AWS using Terraform.
+      </p>
     <p>
       Micro frontends is an architectural style where independently deliverable
       frontend applications are composed into a greater whole. It's useful for
@@ -16,18 +14,12 @@ const About = () => (
       applications that can be delivered to production by multiple teams
       independently.
     </p>
-    <p>
-      To read more about the technique, including a full explanation of how the
+      <p>
+          All code on this website was the work of {' '}
+          <a href="https://twitter.com/thecamjackson">Cam Jackson</a>, who wrote a full explanation of how the
       code for this demo works, check out the{' '}
       <a href="https://martinfowler.com/articles/micro-frontends.html">
         long-form article that Cam wrote for martinfowler.com
-      </a>.
-    </p>
-    <p>
-      If you just want to read the source code for yourself, it's all available
-      on Github at{' '}
-      <a href="https://github.com/micro-frontends-demo">
-        https://github.com/micro-frontends-demo
       </a>.
     </p>
   </main>
